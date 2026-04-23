19596226b75c8f21098781800ed6c4019d65c8fb
duythong88 <duythong.1988@gmail.com>
2026-04-23 16:53:20 +0700

    Add files via upload

---
+++ src/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+
+        let maSp = '@Context.Session.GetInt32("SanPhamDangChon")';
+
+    if (maSp) {
+        moModalChonSize(maSp);
+
+    // clear sau khi dùng
+    fetch('/SanPham/XoaSession');
+    }
+});
+</script>
